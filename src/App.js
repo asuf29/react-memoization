7db64a4767e6cf141f6ceb612dac6a1e127919c1
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-03 00:17:48 +0300

    useMemo hook used

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
+// import "./App.css";
+// import { useState } from "react";
+// import Header from "./components/Header";
+
+// function App() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <div className="App">
+//       <Header number={number < 5 ? 0 : number} />
+
+//       <hr />
+
+//       <h1>{number}</h1>
+//       <button onClick={() => setNumber(number + 1)}>Click</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//useMemo
 import "./App.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  const data = useMemo(() => {
+    return calculateObjects();
+  }, []);
+
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header data={data} />
 
       <hr />
 
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>Click</button>
+
+      <br />
+      <br />
+      <input value={text} onChange={() => setText(target.value)} />
     </div>
   );
+}
+
+function calculateObjects() {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000; i++) {}
+  console.log("Calculating completed!");
+  return { name: "Asu" };
 }
 
 export default App;
