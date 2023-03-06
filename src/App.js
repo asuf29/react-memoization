d1922aa1d2515163effe146db724476274c6a278
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-06 14:46:33 +0300

    App.js codes updated

--- src/App.js
+++ src/App.js
@@ -20,6 +20,44 @@
 // export default App;
 
 //useMemo
+// import "./App.css";
+// import { useState, useMemo } from "react";
+// import Header from "./components/Header";
+
+// function App() {
+//   const [number, setNumber] = useState(0);
+//   const [text, setText] = useState("");
+
+//   const data = useMemo(() => {
+//     return calculateObjects();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Header data={data} />
+
+//       <hr />
+
+//       <h1>{number}</h1>
+//       <button onClick={() => setNumber(number + 1)}>Click</button>
+
+//       <br />
+//       <br />
+//       <input value={text} onChange={() => setText(target.value)} />
+//     </div>
+//   );
+// }
+
+// function calculateObjects() {
+//   console.log("Calculating...");
+//   for (let i = 0; i < 1000000; i++) {}
+//   console.log("Calculating completed!");
+//   return { name: "Asu" };
+// }
+
+// export default App;
+
+//useCallback
 import "./App.css";
 import { useState, useMemo } from "react";
 import Header from "./components/Header";
@@ -28,13 +66,9 @@ function App() {
   const [number, setNumber] = useState(0);
   const [text, setText] = useState("");
 
-  const data = useMemo(() => {
-    return calculateObjects();
-  }, []);
-
   return (
     <div className="App">
-      <Header data={data} />
+      <Header />
 
       <hr />
 
@@ -43,16 +77,9 @@ function App() {
 
       <br />
       <br />
-      <input value={text} onChange={() => setText(target.value)} />
+      <input value={text} onChange={(target) => setText(target.value)} />
     </div>
   );
-}
-
-function calculateObjects() {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000; i++) {}
-  console.log("Calculating completed!");
-  return { name: "Asu" };
 }
 
 export default App;
